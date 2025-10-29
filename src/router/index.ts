14d73a3410e9508c4019ad584adfee43215f3064
Guillermo Gasca <ggasca07@outlook.com>
2025-10-29 16:08:35 -0600

    First commit

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import ProjectsLayout from '@/modules/projects/layouts/ProjectsLayout.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: ProjectsLayout,
+      redirect: { name: 'projects' },
+      children: [
+        {
+          path: 'projects',
+          name: 'projects',
+          component: () => import('@/modules/projects/views/ProjectView.vue'),
+        },
+      ],
+    },
+  ],
+});
+
+export default router;
