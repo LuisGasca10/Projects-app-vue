53912c0e72a107bd065e164be352330ddcb208d7
Guillermo Gasca <ggasca07@outlook.com>
2025-11-12 16:32:49 -0600

    Avnaze

--- src/modules/projects/store/project.store.ts
+++ src/modules/projects/store/project.store.ts
@@ -11,9 +11,19 @@ export const useProjectsStore = defineStore('projects', () => {
     projects.value.push({ id: uuidv4(), name, tasks: [] });
   };
 
+  const addTaskToProject = (projectId: string, taskName: string) => {
+    if (taskName.trim().length === 0) return;
+
+    const project = projects.value.find((p) => p.id === projectId);
+    if (!project) return;
+
+    project.tasks.push({ id: uuidv4(), name: taskName });
+  };
+
   return {
     projectList: computed(() => [...projects.value]),
     noProjects: computed(() => projects.value.length === 0),
     addProject,
+    addTaskToProject,
   };
 });
--- src/modules/projects/views/ProjectView.vue
+++ src/modules/projects/views/ProjectView.vue
@@ -1,5 +1,45 @@
 <template>
-  <bread-crumbs :name="project?.name ?? 'No encontrado'" />
+  <div class="w-full">
+    <section class="m-2">
+      <bread-crumbs :name="project?.name ?? 'No encontrado'" />
+    </section>
+
+    <section class="m-2">
+      <div class="overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th>Comletada</th>
+              <th>Tarea</th>
+              <th>Comletada en</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- row 2 -->
+            <tr v-for="task in project?.tasks" :key="task.id" class="hover:bg-base-300">
+              <th><input type="checkbox" class="checkbox checkbox-primary" /></th>
+              <td>{{ task.name }}</td>
+              <td>{{ task.completedAt }}</td>
+            </tr>
+            <tr class="hover:bg-base-300">
+              <th></th>
+              <td>
+                <input
+                  type="text"
+                  class="input input-primary w-full opacity-60 transition-all hover:opacity-100 focus:opacity-100"
+                  placeholder="Agregar nueva tarea"
+                  @keyup.enter="addTask"
+                  v-model="newTask"
+                />
+              </td>
+              <th></th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,20 +47,32 @@ import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
 import { useProjectsStore } from '../store/project.store';
 import { ref, watch } from 'vue';
 import type { Project } from '../interfaces/project.interface';
+import { useRouter } from 'vue-router';
 
 interface Props {
   id: string;
 }
 
+const router = useRouter();
+const newTask = ref('');
 const props = defineProps<Props>();
 
 const projectStore = useProjectsStore();
 const project = ref<Project | null>();
 
+const addTask = () => {
+  if (!project.value) return;
+  projectStore.addTaskToProject(project.value.id, newTask.value);
+  newTask.value = '';
+};
+
 watch(
   () => props.id,
   () => {
     project.value = projectStore.projectList.find((project) => project.id === props.id);
+    if (!project.value) {
+      router.replace('/');
+    }
   },
   {
     immediate: true,
